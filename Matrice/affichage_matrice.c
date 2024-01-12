dab1bf6cfc2fff38b26360d180df93bf18d25e62
adbaibi ismaïl <63613252+Camy-Devlop@users.noreply.github.com>
2024-01-13 00:24:07 +0100

    Update affichage_matrice.c

--- Matrice/affichage_matrice.c
+++ Matrice/affichage_matrice.c
@@ -1,9 +1,13 @@
-﻿/*
+/*
    auteur: adbaibi ismail
-   date  : 08/01/2024    
+   date  : 11/01/2024    
 */
-
 #include "libmatrice.h"
+/*
+  @brief	 	: 	 affichier la matrice 
+  @param	tab : 	la matrice qu'on veux. affichier 
+  @return :
+*/
 
 void affichage_matrice(t_matrices a)
 {
