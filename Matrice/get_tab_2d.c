ced710037464c9498573a062b526965710395260
adbaibi ismaïl <63613252+Camy-Devlop@users.noreply.github.com>
2024-01-13 18:24:53 +0100

    Update get_tab_2d.c

--- Matrice/get_tab_2d.c
+++ Matrice/get_tab_2d.c
@@ -5,33 +5,33 @@
 
 #include "libmatrice.h"
 /*
-	@brief	 : 	fonction qui va cree un tableau pour la matrice de taille demander 
-				no;bre de colonne et de ligne 
-	
-	@param	l: 	est le no;nre de ligne voulue
-	@param  c: 	nombre de colone
-	
-	@return : 	retourne un tableau de dimation voulue si l ou c sont egaux a 0 il 
-				retourne NULL
+  @brief	 : 	fonction qui va cree un tableau pour la matrice de taille demander 
+        no;bre de colonne et de ligne 
+
+  @param	l: 	est le no;nre de ligne voulue
+  @param  c: 	nombre de colone
+
+  @return : 	retourne un tableau de dimation voulue si l ou c sont egaux a 0 il 
+        retourne NULL
 */
 
 int **get_tab_2d(unsigned int l, unsigned int c)
 {
-	unsigned int	i;
-	
-	i = 0;
-	if (l == 0 || c == 0)
-		return (NULL);
-	int **res = (int **)malloc(l * sizeof(int*));
-	while (i < l)
-	{
-		res[i] = (int *)malloc(c * sizeof(int));
-		if (!res[i])
-		{
-			free_tab(res, i);
-			return (NULL);
-		}
-		i++;
-	}
-	return (res);
+  unsigned int	i;
+
+  i = 0;
+  if (l == 0 || c == 0)
+    return (NULL);
+  int **res = (int **)malloc(l * sizeof(int*));
+  while (i < l)
+  {
+    res[i] = (int *)malloc(c * sizeof(int));
+    if (!res[i])
+    {
+      free_tab(res, i, c);
+      return (NULL);
+    }
+    i++;
+  }
+  return (res);
 } 
