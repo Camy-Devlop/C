77a5285c721712b0134651b7eceba7b8a2ebb08c
adbaibi ismaïl <63613252+Camy-Devlop@users.noreply.github.com>
2024-01-13 18:22:21 +0100

    Update libmatrice.h

--- Matrice/libmatrice.h
+++ Matrice/libmatrice.h
@@ -1,6 +1,6 @@
 /*
    auteur: adbaibi ismail
-   date  : 08/01/2024    
+   date  : 08/01/2024
 */
 
 #ifndef LIBMATRICE_H
@@ -8,37 +8,35 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <string.h>
 
 /*
-  @brief	 	: type matrice avec no;bred e ligne et de colonne 
+  @brief	 	: type matrice avec nombre de ligne et de colonne
 
-  @param	mat	: 	tableau de x dimantion
-  @param	lig	:	nonbre de ligne 
+  @param	mat	: 	tableau de x dimantion d'entier
+  @param	lig	:	nonbre de ligne
   @param	col	:	nombre de colonne
 */
 
-typedef struct s_matrice
- {
-  int 			**matrice;
-  unsigned int 	lig;
-  unsigned int 	col;
+typedef struct s_matrice {
+  int **matrice;
+  unsigned int lig;
+  unsigned int col;
 
- }t_matrice;
+} t_matrice;
 
-void free_tab(int **tab, unsigned int l);
-void free_matrice(t_matrice *a);
+void        memoire_reset_zero(int *memoire, unsigned int n);
+void        free_tab(int **tab, unsigned int l, unsigned int c);
+void        free_matrice(t_matrice *a);
 
-void tab_int_rand(int **tab, unsigned int l, unsigned int c);
+int         **get_tab_2d(unsigned int l, unsigned int c);
+t_matrice   *init(t_matrice *a);
 
-int **get_tab_2d(unsigned int l, unsigned int c);
-t_matrice *init(t_matrice *a);
-
-t_matrice add_matrices(t_matrice *a, t_matrice *b);
-t_matrice multi_matrices(t_matrice *a, t_matrice *b);
-int multi_ligne_colonne(t_matrice *a , t_matrice *b, unsigned int lig, unsigned col);
-
-void replir_ligne(t_matrice *a, int **tab);
-void affichage_matrice(t_matrice a);
+t_matrice   add_matrices(t_matrice *a, t_matrice *b);
+int         multi_ligne_colonne(t_matrice *a, t_matrice *b, unsigned int lig,
+                           unsigned col);
+t_matrice   multi_matrices(t_matrice *a, t_matrice *b);
+void        replir_ligne(t_matrice *a, int **tab);
 
 #endif
