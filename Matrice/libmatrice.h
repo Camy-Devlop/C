68773a8d31b74e9c31c89e2ab35af2b5ed3eee4f
adbaibi ismaïl <63613252+Camy-Devlop@users.noreply.github.com>
2024-01-16 10:48:43 +0100

    Update libmatrice.h
    Ajout de fonction det sub

--- Matrice/libmatrice.h
+++ Matrice/libmatrice.h
@@ -23,20 +23,25 @@ typedef struct s_matrice {
   int **matrice;
   unsigned int lig;
   unsigned int col;
+  int          *det;
 
 } t_matrice;
 
-void				tab_int_rand(int **tab, unsigned int l, unsigned int c);
-void				memoire_reset_zero(int *memoire, unsigned int n);
-void				free_tab(int **tab, unsigned int l, unsigned int c);
-void				free_matrice(t_matrice *a);
+void        memoire_reset_zero(int *memoire, unsigned int n);
+void        free_tab(int **tab, unsigned int l, unsigned int c);
+void        free_matrice(t_matrice *a);
 
-int					**get_tab_2d(unsigned int l, unsigned int c);
-t_matrice		*init(t_matrice *a);
+int         **get_tab_2d(unsigned int l, unsigned int c);
+t_matrice   *init(t_matrice *a);
 
-t_matrice		add_matrices(t_matrice *a, t_matrice *b);
-int					multi_ligne_colonne(t_matrice *a, t_matrice *b, unsigned int lig, unsigned col);
-t_matrice		multi_matrices(t_matrice *a, t_matrice *b);
-void				replir_ligne(t_matrice *a, int **tab);
+t_matrice   add_matrices(t_matrice *a, t_matrice *b);
+t_matrice   sub_matrices(t_matrice *a, t_matrice *b);
+
+t_matrice   det_matrices(t_matrice *a);
+
+int         multi_ligne_colonne(t_matrice *a, t_matrice *b, unsigned int lig,
+                           unsigned col);
+t_matrice   multi_matrices(t_matrice *a, t_matrice *b);
+void        replir_ligne(t_matrice *a, int **tab);
 
 #endif
