9a67f364eeac1eda3b48a41def4bfb32c2b5741f
adbaibi ismaïl <63613252+Camy-Devlop@users.noreply.github.com>
2024-01-16 08:45:52 +0100

    Update main.c

--- Matrice/main.c
+++ Matrice/main.c
@@ -1,31 +1,47 @@
 /*
-     auteur: adbaibi ismail
-     date  : 13/01/2024    
+         auteur: adbaibi ismail
+         date  : 08/01/2024
 */
 
 #include "libmatrice.h"
-int main()
- {
-     t_matrice a;
-     a.lig = 4;
-     a.col = 4;
-     if (init(&a) != NULL)
-     {
-         int **n;
-         n = get_tab_2d(4, 4);
-         tab_int_rand(n, a.lig, a.col);
-         replir_ligne(&a,n);
-         affichage_matrice(a);
-         t_matrice c = add_matrices(&a, &a);
-         printf("=========add matrice avec elle meme ==========\n");
-        affichage_matrice(c);
-        c = multi_matrices(&a, &c);
-        printf("=========multiplication de la matrice avec c ==========\n");
-        affichage_matrice(c);
+int main() {
+  t_matrice a;
+  t_matrice b;
+  a.lig = 2;
+  a.col = 2;
+  b.lig = 2;
+  b.col = 2;
 
-        free_matrice(&a);
-        free_matrice(&c);
-        free_tab(n, 4,4);
-    }else
-        printf("ERROR");
- }
+  init(&a);
+  init(&b);
+
+  if (init(&a) != NULL) {
+    int **n;
+    int **m;
+    n = get_tab_2d(a.lig, a.col);
+    m = get_tab_2d(b.lig, b.col);
+    tab_int_rand(n, a.lig, a.col);
+    tab_int_rand(m, b.lig, b.col);
+
+    replir_ligne(&a, n);
+    replir_ligne(&b, m);
+    affichage_matrice(a);
+    printf("\n");
+
+    affichage_matrice(b);
+
+    t_matrice c = add_matrices(&a, &a);
+    printf("=========add matrice avec elle meme ==========\n");
+    affichage_matrice(c);
+    c = multi_matrices(&a, &b);
+    printf("=========multiplication de la matrice avec c ==========\n");
+    affichage_matrice(c);
+
+    free_matrice(&a);
+    free_matrice(&b);
+    free_matrice(&c);
+    free_tab(n, a.lig, a.col);
+    free_tab(m, b.lig, b.col);
+  } else
+    printf("ERROR");
+}
