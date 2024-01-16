7ac5ba2b3878aa691133859776a9232494ec97da
adbaibi ismaïl <63613252+Camy-Devlop@users.noreply.github.com>
2024-01-16 08:40:38 +0100

    Update multi_ligne_colonne.c

--- Matrice/multi_ligne_colonne.c
+++ Matrice/multi_ligne_colonne.c
@@ -1,33 +1,34 @@
 /*
    auteur: adbaibi ismail
-   date  : 10/01/2024    
+   date  : 10/01/2024
 */
-
 
 #include "libmatrice.h"
 /*
-  @brief	 : 	fonction qui va faire la multiplication matricielle avec la 
-				une ligne et la colone demander lig et col
+  @breif	 : 	fonction qui va multiplication sur une ligne et sur une
+  colonne
 
-  @param	a: liberé la matrice de la memoire 
+  @param a    :	est la matrice
+  @param b    :	est la matrice
+  @param lig  :	quelles ligne doit subire la multiplication
+  @param col  :	quelle colonne doit subire la multiplication
 
-  @return : retourne un le resultat de la multiplication 
+
+  @return int :	retourne un entier qui est ma multiplication de de la lignc                  et de la colonnne
 */
 
-int multi_ligne_colonne(t_matrice *a , t_matrice *b, unsigned int lig, unsigned col)
-{
-	unsigned int	i;
-	int 			res;
-	i = 0 ;
-	res = 0;
-    // verifier si tout est conforme a la demander dans les parametre
-	if (0 == a->col || 0 == a->lig || a->lig != b->lig || a->col != b->col || !a->matrice ||!b->matrice)
-		return (NULL);
-    // litiplication de la ligne avec la colonne
-	while (i < a->col)
-	{
-		res =  res + (a->matrice[lig][i] * b->matrice[i][col]);
-		i++;
-	}
-	return res;
+int multi_ligne_colonne(t_matrice *a, t_matrice *b, unsigned int lig,
+                           unsigned col) {
+  unsigned int i;
+  int res;
+  i = 0;
+  res = 0;
+  if (0 == a->col || 0 == a->lig || a->col != b->lig ||
+      !a->matrice || !b->matrice)
+    return (NULL);
+  while (i < a->col) {
+    res = res + (a->matrice[lig][i] * b->matrice[i][col]);
+    i++;
+  }
+  return res;
 }
